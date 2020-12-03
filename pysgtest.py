b8806c3433ba1b22b18010af37014a4a5248701d
Mookiefer <Mookiefer@users.noreply.github.com>
2020-12-02 23:01:36 -0500

    Change theme & add exit button

--- pysgtest.py
+++ pysgtest.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as PySG
 
+PySG.theme('LightGreen2')
 
 layout = [[
     PySG.Input("First"),
@@ -9,13 +10,17 @@ layout = [[
     PySG.Input("Third", use_readonly_for_disable=False, disabled=True),
     ], [
     PySG.Text("Fourth"),
+    ], [
+    PySG.Exit(),
     ]]
 
 window = PySG.Window("Window", layout=layout)
 
 while True:
     event, values = window.read()
-    if event is None:
+    if event is PySG.WIN_CLOSED:
+        break
+    if event == "Exit":
         break
 
 window.close()
